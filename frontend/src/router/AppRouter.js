9cc16ad2069026c2169e88d16074631f22bd4a56
EaintHmueThapye <scm.eainthmue@gmail.com>
2023-01-06 15:18:08 +0630

    [user_create_update_form] fix login design and create common forms for user create and update.

--- frontend/src/router/AppRouter.js
+++ frontend/src/router/AppRouter.js
@@ -3,11 +3,11 @@ import { Switch, Redirect, withRouter } from "react-router-dom";
 import PublicRoute from "./PublicRoute";
 import PrivateRoute from "./PrivateRoute";
 import LoginPage from "../pages/Login/LoginPage";
-import RegisterPage from "../pages/Register/RegisterPage";
 import HomePage from "../pages/Home/HomePage";
 import EventListPage from "../pages/EventList/EventListPage";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import CreatePage from "../pages/Create/CreatePage";
 
 const AppRouter = withRouter(({ location }) => {
 
@@ -19,19 +19,20 @@ const AppRouter = withRouter(({ location }) => {
   return (
     <>
       {
-        (location.pathname !== '/admin/login' && location.pathname !== '/admin/register') && <Header showMenu={true} />
+        (location.pathname !== '/admin/login' && location.pathname !== '/user/create') && <Header showMenu={true} />
       }
       <Switch>
         <PublicRoute path="/admin/login" component={LoginPage} />
         <PrivateRoute path="/admin/home" component={HomePage} />
         <PrivateRoute path="/admin/events" component={EventListPage} />
-        <PublicRoute path="/admin/register" component={RegisterPage} />
+        <PublicRoute path="/user/create" component={CreatePage} />
+        <PrivateRoute path="/user/1/update" component={CreatePage} />
 
         {/* Redirect all 404's to home */}
         <Redirect to='/admin/login' />
       </Switch>
       {
-        (location.pathname !== '/admin/login' && location.pathname !== '/admin/register') && <Footer />
+        (location.pathname !== '/admin/login' && location.pathname !== '/user/create') && <Footer />
       }
     </>
   )
