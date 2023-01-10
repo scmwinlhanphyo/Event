0310e9d446bd67b39eff2e6a8052d8fe4ef8c572
scmwinlhanphyo <win.lhanphyo@seattleconsultingmyanmar.com>
2023-01-10 09:39:03 +0630

    change password forget password

--- frontend/src/router/AppRouter.js
+++ frontend/src/router/AppRouter.js
@@ -3,12 +3,13 @@ import { Switch, Redirect, withRouter } from "react-router-dom";
 import PublicRoute from "./PublicRoute";
 import PrivateRoute from "./PrivateRoute";
 import LoginPage from "../pages/Login/LoginPage";
+import ForgetPassword from "../pages/ForgetPassword/ForgetPassword";
+import PasswordChangeForm from "../pages/PasswordChangeForm/PasswordChangeForm";
 import UserPage from "../pages/User/UserPage";
 import EventPage from "../pages/Event/EventPage";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import CreatePage from "../pages/Create/CreatePage";
-import ProfilePage from "../pages/Profile/ProfilePage";
 
 const AppRouter = withRouter(({ location }) => {
 
@@ -18,21 +19,22 @@ const AppRouter = withRouter(({ location }) => {
   return (
     <>
       {
-        (location.pathname !== '/admin/login' && location.pathname !== '/user/create') && <Header showMenu={true} />
+        (location.pathname !== '/admin/login' && location.pathname !== '/admin/forgetPassword' && location.pathname !== '/admin/PasswordChangeForm' && location.pathname !== '/user/create') && <Header showMenu={true} />
       }
       <Switch>
         <PublicRoute path="/admin/login" component={LoginPage} />
+        <PublicRoute path="/admin/forgetPassword" component={ForgetPassword} />
+        <PublicRoute path="/admin/PasswordChangeForm" component={PasswordChangeForm} />
         <PrivateRoute path="/admin/users" component={UserPage} />
         <PrivateRoute path="/admin/events" component={EventPage} />
         <PublicRoute path="/admin/user/create" component={CreatePage} />
         <PrivateRoute path="/admin/user/:id/update" component={CreatePage} />
-        <PrivateRoute path="/admin/profile" component={ProfilePage} />
 
         {/* Redirect all 404's to home */}
         <Redirect to='/admin/login' />
       </Switch>
       {
-        (location.pathname !== '/admin/login' && location.pathname !== '/user/create') && <Footer />
+        (location.pathname !== '/admin/login' && location.pathname !== '/admin/forgetPassword' && location.pathname !== '/admin/PasswordChangeForm' && location.pathname !== '/user/create') && <Footer />
       }
     </>
   )
